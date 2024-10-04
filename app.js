ae918f1b0e8ef10e3203cea4bbb237f43c311ce9
Hardik Syal <128090107+HardikGH@users.noreply.github.com>
2024-10-04 18:48:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
-let command = prompt("What would you like to do?")
-const list = [];
+const p1 = {
+    score: 0,
+    button: document.querySelector("#p1Button"),
+    display: document.querySelector("#p1Display")
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector("#p2Button"),
+    display: document.querySelector("#p2Display")
+}
 
-while(command !== "quit" && command !== "q"){
-if(command === "new"){
-    let task = prompt("Enter new task")
-    list.push(task);
-    console.log(`${task} added to the list`)
-}
-else if(command === "list"){
-    console.log("********");
-    for(let i=0 ; i<list.length ; i++){
-        console.log(`${i}: ${list[i]}`);
+const resetButton = document.querySelector("#reset")
+const winningScoreSelect = document.querySelector('#gamePoint')
+let winningScore = 5;
+let isGameOver = false;
+
+
+function updateScores(player , opponent){
+    if(!isGameOver){
+        player.score += 1
+        if(player.score === winningScore){
+            isGameOver = true;
+            player.display.classList.add('has-text-success')
+            opponent.display.classList.add('has-text-danger')
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
     }
-    console.log("********");
-}
-else if(command === "delete"){
-    const index = parseInt(prompt("Enter the index number of the item you want to delete."));
-    if(!Number.isNaN(index)){
-        const deleted = list.splice(index,1);
-        console.log(`Deleted ${deleted[0]}`)
+} 
+p1.button.addEventListener('click' , function(){
+    updateScores(p1,p2);
+})
+
+p2.button.addEventListener('click' , function(){
+    updateScores(p2,p1);
+})
+
+winningScoreSelect.addEventListener('change' , function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+ isGameOver = false;
+    for(let p of [p1,p2]){
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false
     }
-    else{ 
-        console.log("Unknown index") 
-    }
 }
-else{
-    console.log("Enter a valid Input!")
-}
-    command = prompt("What would you like to do?")
-}
-console.log("QUITING THE APP.")
